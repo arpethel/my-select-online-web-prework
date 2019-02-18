5867568a6ba454a2de649e54bea65b2a1cfb403e
Ryan Pethel <arpethel@gmail.com>
2019-02-18 23:23:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,22 @@
-def my_select(collection)
+def my_select(array)
   i = 0
-
   my_arr = []
 
-  while i < collection.length
-    block_given?
-    my_arr << yield(collection[i])
+  while i < array.length
+    if yield(array[i]) == true
+      my_arr.push(array[i])
+    end
     i += 1
   end
   my_arr
 end
 
-  # collection.select { |item|  }
-end
+#   if yield(array[count]) == true
+#   result.push(array[count])
+# end
+#   count+=1
+# end
+# result
+# 
+#   # collection.select { |item|  }
+# end
